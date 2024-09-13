62aa350ba490aa2298e1353375611b5619468bf1
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-13 18:51:41 +0200

    created two components

---
+++ src/AppHero.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    name: 'AppHero',
+  }
+</script>
+
+<template>
+  <p>Ciao sono un jumbotron!</p>
+</template>
+
+<style scoped>
+</style>
---
+++ src/AppTitle.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    name: 'AppTitle',
+  }
+</script>
+
+<template>
+  <h1>La mia prima app con Vite</h1>
+</template>
+
+<style scoped>
+</style>
